22fcb2a5f5f032d4ae408090988e52274f8fa1e2
Islay Anderson <me@islayanderson.co.uk>
2025-01-10 00:24:03 +0000

    Update ubuntu-test.sh

--- ubuntu-test.sh
+++ ubuntu-test.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 sudo apt update
-sudo apt upgrade
-sudo apt install ssh
-sudo systemctl enable ssh
-sudo apt install net-tools
-sudo apt install curl
-sudo apt install git
-sudo apt install npm
-sudo apt install docker.io
-sudo apt install apache2
-sudo ufw allow in "Apache"
-sudo ufw allow 80,443,3000,996,7946,4789,2377/tcp; ufw allow 7946,4789,2377/udp;
-sudo apt install mysql-server
-sudo mysql <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-exit
-EOF
-sudo mysql_secure_installation <<EOF
+sudo apt upgrade <<EOF
 y
-1
+EOF
+sudo apt install ssh <<EOF
+y
+EOF
+sudo systemctl enable ssh
+sudo apt install net-tools <<EOF
+y
+EOF
+sudo apt install curl <<EOF
+y
+EOF
+sudo apt install git <<EOF
+y
+EOF
+sudo apt install npm <<EOF
+y
+EOF
+sudo apt install docker.io <<EOF
+y
+EOF
+sudo apt install apache2 <<EOF
 y
 EOF
 sudo apt install php libapache2-mod-php php-mysql
 sudo systemctl restart apache2
-docker run -p 80:80 -p 443:443 -p 3000:3000 -e ACCEPTED_TERMS=true -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain caprover/caprover
-npm install -g caprover
-caprover serversetup
+sudo docker run -p 80:80 -p 443:443 -p 3000:3000 -e ACCEPTED_TERMS=true -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain caprover/caprover
+sudo npm install -g caprover
+sudo caprover serversetup <<EOF
+y
+192.168.11.70
+EOF
